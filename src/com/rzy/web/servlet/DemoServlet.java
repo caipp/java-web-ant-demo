a3ca76c27cb9a3a1e572d66ef5b79496ee669e7c
caipiaoping <125465729@qq.com>
2019-01-03 15:41:21 +0800

    ant build

--- src/com/rzy/web/servlet/DemoServlet.java
+++ src/com/rzy/web/servlet/DemoServlet.java
@@ -8,12 +8,10 @@ import javax.servlet.http.HttpServletResponse;
 
 public class DemoServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    //这里是处理方法，表示当访问URI为webapp1/demo/demo时，转到demo.jsp文件显示 
     req.getRequestDispatcher("/jsp/demo.jsp").forward(req,resp);
     }
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    //这里在doPost方法里调用doGet方法，目的是无论表单以get方式传递还是post方式传递，处理方法都一致。
         doGet(req, resp);
     }
 
--- src/com/rzy/web/servlet/ResultServlet.java
+++ src/com/rzy/web/servlet/ResultServlet.java
@@ -8,13 +8,13 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ResultServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    //通过name属性获取请求的参数
+    
       String name = req.getParameter("name");
       String pwd  = req.getParameter("pwd");
-    //通过setAttribute才能将获取的参数传递出去，转到jsp处理
+    
       req.setAttribute("name",name);
       req.setAttribute("pwd", pwd);
-    //这里是处理方法，表示当访问URI为webapp1/demo/result时，转到result.jsp文件显示  
+      
       req.getRequestDispatcher("/jsp/result.jsp").forward(req,resp);
     }
 
